a785cd9397a53adf7b53df77bfdf874cd4263c46
Zachry2906 <aryazakaria67@gmail.com>
2023-11-20 08:00:12 +0700

    ganti nama account

--- dist/koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-    $hostname = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "ria";
-
-    $connect = mysqli_connect($hostname, $username, $password, $database);
-?>
